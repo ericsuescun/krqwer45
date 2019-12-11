e75db5ba3a59020cd6af4768ac9e72ea8d18dd3b
Eric Suescun <edsuescun@gmail.com>
2019-12-11 15:08:40 -0500

    Se implementa el toggler de tareas.

--- src/App.js
+++ src/App.js
@@ -11,19 +11,52 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      errorStatus: ''
     }
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if(this.state.newTask === '') {
+      this.setState({ errorStatus: 'error' });
+      console.log('error');
+    } else {
+      const l = this.state.tasks.length;
+      const lastId = this.state.tasks[l - 1].id;
+      let oldTasks = this.state.tasks;
+      let newTask = {id: lastId + 1, name: this.state.newTask, done: false};
+      console.log(newTask);
+      this.setState({ tasks: [...oldTasks, newTask], newTask: '', errorStatus: '' });
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ newTask: e.target.value})
+  }
+
+  handleClick(index,e) {
+    console.log(index);
+    let flipTask = this.state.tasks;
+    flipTask.map((task, n) => {
+      if(index === n) {
+        task.done = !task.done;
+        console.log(task);
+      }
+    });
+    this.setState({tasks: flipTask});
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li className={task.done === true ? 'done' : ''} key={task.id} onClick={this.handleClick.bind(this, index)}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.handleChange.bind(this)} className={this.state.errorStatus}/>
           </form>
         </div>
       </div>
